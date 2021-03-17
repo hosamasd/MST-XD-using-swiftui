64c42554beca7e32665f9732584b598b499f6bf2
hosam <hosamelmalt@gmail.com>
2021-03-17 07:33:00 +0200

    Initial Commit

---
+++ MST XD/MST_XDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MST_XDApp.swift
+//  MST XD
+//
+//  Created by hosam on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MST_XDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
